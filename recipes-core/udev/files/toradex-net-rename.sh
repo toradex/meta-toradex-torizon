a099cab221f91c3e0408912008749520ff361c7f
Abdur Rehman <abdur.rehman@toradex.com>
2019-09-18 08:51:45 +0200

    udev-toradex-rules: rename toradex net devices
    Add rule to assign predictable names for net devices with Toradex OUI.
    Assign name "ethernet0" to the device that matches the serial number and
    "ethernet1" to the device that matches serial number + 0x100000
    
    Related-to: #54695
    
    Signed-off-by: Abdur Rehman <abdur.rehman@toradex.com>

---
+++ recipes-core/udev/files/toradex-net-rename.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# Assign name "ethernetN" to net devices with Toradex OUI.
+# Supports Toradex boards with single/dual ethernet interfaces.
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 KERNEL" >&2
+    exit 1
+fi
+
+dev="${1}"
+devpath="/sys/class/net/${dev}"
+if [ ! -e ${devpath} ]
+then
+  echo "Error: dev path does not exist!" >&2
+  exit 1
+fi
+
+devaddr="${devpath}/address"
+
+# Toradex OUI
+oui=00:14:2d
+
+# Number of net devices with Toradex OUI
+devs=$(grep ${oui} /sys/class/net/*/address | wc -l)
+
+if [ ${devs} -gt 2 ]
+then
+  # Limitation: this script can handle up to two devices
+  echo "Error: Found ${devs} devices with Toradex OUI!" >&2
+  echo "Error: Up to 2 devices are supported at the moment" >&2
+  exit 1
+fi
+
+mac0_serial=$(cat /proc/device-tree/serial-number | tr '\0' '\n')
+
+# Secondary MAC address is allocated from block
+# 0x100000(1048576) higher than the first MAC address
+mac1_offset=1048576
+mac1_serial=$((mac0_serial + mac1_offset))
+
+for i in $(seq 0 $((devs - 1)))
+do
+  eval serial="\${mac${i}_serial}"
+
+  # 16777215 = 0xFFFFFF, maximum valid serial
+  if [ ${serial} -gt 16777215 ]
+  then
+    echo "Error: mac${i} serial is greater than 0xFFFFFF!" >&2
+    exit 1
+  fi
+  eval mac${i}_serial_hex=$(eval printf "%x" "\${mac${i}_serial}" |  sed -e 's/[0-9a-f]\{2\}/&:/g' -e 's/:$//')
+  eval mac${i}="${oui}:\${mac${i}_serial_hex}"
+
+  eval currentmac="\${mac${i}}"
+  if [ $(cat ${devaddr}) = ${currentmac} ]
+  then
+    echo "ethernet${i}"
+    exit 0
+  fi
+done
+
+echo "Error: Could not match MAC address!" >&2
+exit 1
--- recipes-core/udev/udev-toradex-rules.bb
+++ recipes-core/udev/udev-toradex-rules.bb
@@ -2,10 +2,16 @@ LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 SRC_URI = "\
+    file://10-toradex-net-rename.rules \
     file://99-toradex.rules \
+    file://toradex-net-rename.sh \
 "
 
 do_install () {
     install -d ${D}${sysconfdir}/udev/rules.d
+    install -m 0644 ${WORKDIR}/10-toradex-net-rename.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0644 ${WORKDIR}/99-toradex.rules ${D}${sysconfdir}/udev/rules.d/
+
+    install -d ${D}${bindir}
+    install -m 0755 ${WORKDIR}/toradex-net-rename.sh ${D}${bindir}/
 }
