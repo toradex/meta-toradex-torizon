toradex/meta-toradex-torizon

commit 059dbfee8724019ce0a3f5a2bcf9aad394ed7944

recipes-core: udev: remove emmc rules as they are now live in BSP
The eMMC rules got moved to BSP layer and are no longer required in
torizon meta-layer.

Related-to: ELB-2380, ELB-2696
Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>

recipes-core/udev/files/99-toradex-persistent-emmc-naming.rules
@@ -1,1 +1,0 @@
-

recipes-core/udev/udev-toradex-rules.bbappend
@@ -6,7 +6,6 @@ SRC_URI += "\
     file://91-toradex-i2cdev.rules \
     file://92-toradex-spidev.rules \
     file://toradex-net-rename.sh \
-    file://99-toradex-persistent-emmc-naming.rules \	
 "
 
 do_install_append () {
@@ -14,8 +13,6 @@ do_install_append () {
     install -m 0644 ${WORKDIR}/90-toradex-gpio.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0644 ${WORKDIR}/91-toradex-i2cdev.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0644 ${WORKDIR}/92-toradex-spidev.rules ${D}${sysconfdir}/udev/rules.d/
-    
-    install -m 0644 ${WORKDIR}/99-toradex-persistent-emmc-naming.rules ${D}${sysconfdir}/udev/rules.d/
 
     install -d ${D}${bindir}
     install -m 0755 ${WORKDIR}/toradex-net-rename.sh ${D}${bindir}/

recipes-core/udev/files/apalis-imx6/99-toradex-persistent-emmc-naming.rules
@@ -1,4 +1,0 @@
-KERNEL=="mmcblk[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="2198000.usdhc", SYMLINK+="emmc"
-KERNEL=="mmcblk[0-9]boot[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="2198000.usdhc", SYMLINK+="emmc-boot%n"
-KERNEL=="mmcblk[0-9]p[0-9]", ENV{DEVTYPE}=="partition", KERNELS=="2198000.usdhc", SYMLINK+="emmc-part%n"
-

recipes-core/udev/files/apalis-imx8/99-toradex-persistent-emmc-naming.rules
@@ -1,4 +1,0 @@
-KERNEL=="mmcblk[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="5b010000.usdhc", SYMLINK+="emmc"
-KERNEL=="mmcblk[0-9]boot[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="5b010000.usdhc", SYMLINK+="emmc-boot%n"
-KERNEL=="mmcblk[0-9]p[0-9]", ENV{DEVTYPE}=="partition", KERNELS=="5b010000.usdhc", SYMLINK+="emmc-part%n"
-

recipes-core/udev/files/apalis-imx8x/99-toradex-persistent-emmc-naming.rules
@@ -1,4 +1,0 @@
-KERNEL=="mmcblk[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="5b010000.usdhc", SYMLINK+="emmc"
-KERNEL=="mmcblk[0-9]boot[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="5b010000.usdhc", SYMLINK+="emmc-boot%n"
-KERNEL=="mmcblk[0-9]p[0-9]", ENV{DEVTYPE}=="partition", KERNELS=="5b010000.usdhc", SYMLINK+="emmc-part%n"
-

recipes-core/udev/files/colibri-imx6/99-toradex-persistent-emmc-naming.rules
@@ -1,4 +1,0 @@
-KERNEL=="mmcblk[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="2198000.usdhc", SYMLINK+="emmc"
-KERNEL=="mmcblk[0-9]boot[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="2198000.usdhc", SYMLINK+="emmc-boot%n"
-KERNEL=="mmcblk[0-9]p[0-9]", ENV{DEVTYPE}=="partition", KERNELS=="2198000.usdhc", SYMLINK+="emmc-part%n"
-

recipes-core/udev/files/colibri-imx7/99-toradex-persistent-emmc-naming.rules
@@ -1,4 +1,0 @@
-KERNEL=="mmcblk[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="30b60000.usdhc", SYMLINK+="emmc"
-KERNEL=="mmcblk[0-9]boot[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="30b60000.usdhc", SYMLINK+="emmc-boot%n"
-KERNEL=="mmcblk[0-9]p[0-9]", ENV{DEVTYPE}=="partition", KERNELS=="30b60000.usdhc", SYMLINK+="emmc-part%n"
-

recipes-core/udev/files/colibri-imx8x/99-toradex-persistent-emmc-naming.rules
@@ -1,4 +1,0 @@
-KERNEL=="mmcblk[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="5b010000.usdhc", SYMLINK+="emmc"
-KERNEL=="mmcblk[0-9]boot[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="5b010000.usdhc", SYMLINK+="emmc-boot%n"
-KERNEL=="mmcblk[0-9]p[0-9]", ENV{DEVTYPE}=="partition", KERNELS=="5b010000.usdhc", SYMLINK+="emmc-part%n"
-

commit 18c85de1d3513bacc23369d0a21aeac4dd1224d2

udev-toradex-rules: add back 99-toradex-persistent-emmc-naming.rules
A regression was introduced by commit 059dbfee:
[ recipes-core: udev: remove emmc rules as they are now live in BSP ]

for colibri-imx7 machine, since torizon supports both emmc and nand
devices, we need this 99-toradex-persistent-emmc-naming.rules here, or
else the emmc-part* symbolic links would not be created during boot.

Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-core/udev/files/99-toradex-persistent-emmc-naming.rules
@@ -1,0 +1,3 @@
+KERNEL=="mmcblk[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="30b60000.usdhc", SYMLINK+="emmc"
+KERNEL=="mmcblk[0-9]boot[0-9]", ENV{DEVTYPE}=="disk", KERNELS=="30b60000.usdhc", SYMLINK+="emmc-boot%n"
+KERNEL=="mmcblk[0-9]p[0-9]", ENV{DEVTYPE}=="partition", KERNELS=="30b60000.usdhc", SYMLINK+="emmc-part%n"

recipes-core/udev/udev-toradex-rules.bbappend
@@ -1,11 +1,14 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
-SRC_URI += "\
+SRC_URI_append = " \
     file://10-toradex-net-rename.rules \
     file://90-toradex-gpio.rules \
     file://91-toradex-i2cdev.rules \
     file://92-toradex-spidev.rules \
     file://toradex-net-rename.sh \
+"
+SRC_URI_append_colibri-imx7 = " \
+    file://99-toradex-persistent-emmc-naming.rules \
 "
 
 do_install_append () {
@@ -16,4 +19,8 @@ do_install_append () {
 
     install -d ${D}${bindir}
     install -m 0755 ${WORKDIR}/toradex-net-rename.sh ${D}${bindir}/
+}
+
+do_install_append_colibri-imx7 () {
+    install -m 0644 ${WORKDIR}/99-toradex-persistent-emmc-naming.rules ${D}${sysconfdir}/udev/rules.d/
 }