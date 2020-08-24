toradex/meta-toradex-torizon

commit 20e128389d36ce692fab1b7d0b56e4ecfc1badf6

sudo: change lmp to torizon
We dont have a lmp user, change it to be torizon.

Related-to: TOR-1207
Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-extended/sudo/sudo_%.bbappend
@@ -1,15 +1,17 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/${BPN}:"
 
 SRC_URI_append = " \
-    file://sudoers.lmp \
+    file://sudoers.torizon \
 "
 
 # We explicitly require pam
 inherit features_check
 
+PACKAGECONFIG_append = " pam-wheel"
+
 REQUIRED_DISTRO_FEATURES = "pam"
 
 do_install_append () {
     install -m 0755 -d ${D}${sysconfdir}/sudoers.d
-    install -m 0440 ${WORKDIR}/sudoers.lmp ${D}${sysconfdir}/sudoers.d/lmp
+    install -m 0440 ${WORKDIR}/sudoers.torizon ${D}${sysconfdir}/sudoers.d/torizon
 }

commit 8aab99835895a5b7427bae00dc24bea026f0dc0c

sudo: drop pam-wheel
A regression was introduced by commit 20e12838:
[ sudo: change lmp to torizon ]

PACKAGECONFIG pam-wheel leads to the sudoers torizon doesn't work any
more, let's drop it.

Related-to: TOR-1338
Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-extended/sudo/sudo_%.bbappend
@@ -7,8 +7,6 @@ SRC_URI_append = " \
 # We explicitly require pam
 inherit features_check
 
-PACKAGECONFIG_append = " pam-wheel"
-
 REQUIRED_DISTRO_FEATURES = "pam"
 
 do_install_append () {