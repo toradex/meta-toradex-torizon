toradex/meta-toradex-torizon

commit 49782af8b06da101c6d6a4899e34ef079cc4e934

systemd: package systemd specific udev rules to udev package
Workaround for some systemd specific udev rules being packaged in
systemd package while they are needed by initramfs which doesn't want
install systemd.

Please refer to the discussion:
https://www.mail-archive.com/openembedded-core@lists.openembedded.org/msg140195.html

Fix it by splitting systemd specific udev rules to its own package, and
let initramfs-module-plymouth RDEPENDS on it.

Related-to: TOR-1324
Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-core/systemd/systemd_%.bbappend
@@ -5,6 +5,21 @@ SRC_URI_append_genericx86-64 = " file://0001-rules-whitelist-hd-devices.patch"
 PACKAGECONFIG_append = " resolved networkd"
 RRECOMMENDS_${PN}_remove = "os-release"
 
+# Workaround for some systemd specific udev rules being packaged in
+# systemd package while they are needed by initramfs which doesn't
+# want install systemd. Please refer to the discussion:
+# https://www.mail-archive.com/openembedded-core@lists.openembedded.org/msg140195.html
+#
+# Fix it by splitting systemd specific udev rules to its own package,
+# which could be installed by initramfs.
+PACKAGES_prepend = "${PN}-udev-rules "
+FILES_${PN}-udev-rules = " \
+    ${rootlibexecdir}/udev/rules.d/70-uaccess.rules \
+    ${rootlibexecdir}/udev/rules.d/71-seat.rules \
+    ${rootlibexecdir}/udev/rules.d/73-seat-late.rules \
+    ${rootlibexecdir}/udev/rules.d/99-systemd.rules \
+"
+
 DEF_FALLBACK_NTP_SERVERS="time.cloudflare.com time1.google.com time2.google.com time3.google.com time4.google.com"
 EXTRA_OEMESON += ' \
 	-Dntp-servers="${DEF_FALLBACK_NTP_SERVERS}" \

recipes-core/initramfs-framework/initramfs-framework_1.0.bbappend
@@ -14,7 +14,7 @@ RDEPENDS_initramfs-module-psplash = "${PN}-base psplash"
 FILES_initramfs-module-psplash = "/init.d/00-psplash"
 
 SUMMARY_initramfs-module-plymouth = "initramfs support for plymouth"
-RDEPENDS_initramfs-module-plymouth = "${PN}-base plymouth"
+RDEPENDS_initramfs-module-plymouth = "${PN}-base plymouth ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'systemd-udev-rules', '', d)}"
 FILES_initramfs-module-plymouth = "/init.d/02-plymouth"
 
 do_install_append() {

commit 9897bf03e333d2cb4a5837051b8cf769350045b6

systemd: let systemd RDEPENDS on systemd-udev-rules
A regression was introduced by commit 49782af8:
[ systemd: package systemd specific udev rules to udev package ]

it split systemd related udev rules out to its own package but did not
let systemd RDEPENDS on it, which causes these udev rules are missing
in rootfs.

Related-to: TOR-1324
Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-core/base-files/base-files_%.bbappend
@@ -5,7 +5,7 @@ hostname = ""
 do_install_append_qemuarm64 () {
     cat >> ${D}${sysconfdir}/fstab <<EOF
 
-/dev/vda1            /mnt/boot            vfat       nofail,noatime,sync   0  0
+/dev/vda1            /mnt/boot            vfat       noatime,sync   0  0
 EOF
 }
 

recipes-core/systemd/systemd_%.bbappend
@@ -13,6 +13,7 @@ RRECOMMENDS_${PN}_remove = "os-release"
 # Fix it by splitting systemd specific udev rules to its own package,
 # which could be installed by initramfs.
 PACKAGES_prepend = "${PN}-udev-rules "
+RDEPENDS_${PN} += "systemd-udev-rules"
 FILES_${PN}-udev-rules = " \
     ${rootlibexecdir}/udev/rules.d/70-uaccess.rules \
     ${rootlibexecdir}/udev/rules.d/71-seat.rules \