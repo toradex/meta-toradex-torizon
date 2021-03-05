toradex/meta-toradex-torizon

commit 3f3faa9ff024ff35c4e3956333750b80bb35e582

ostree-kernel-initramfs: add ostree-devicetree-overlays package
Split devicetree overlays and overlays.txt to a standalone package
ostree-devicetree-overlays, this is needed because we only install
overlays.txt to ostree rootfs when fitImage is enabled, in which case,
ostree-devicetrees would not be installed, so we need a extra package
to contain overlays.txt.

Also drop reference to device-tree-overlay-filter, it has been dropped
from Toradex BSP meta layer.

Related-to: TOR-1700

Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-sota/ostree-kernel-initramfs/ostree-kernel-initramfs_%.bbappend
@@ -1,3 +1,7 @@
+PACKAGES_prepend = "ostree-devicetree-overlays "
+ALLOW_EMPTY_ostree-devicetree-overlays = "1"
+FILES_ostree-devicetree-overlays = "${nonarch_base_libdir}/modules/*/dtb/*.dtbo ${nonarch_base_libdir}/modules/*/dtb/overlays.txt"
+
 do_install_append () {
     if [ ${@ oe.types.boolean('${OSTREE_DEPLOY_DEVICETREE}')} = True ]; then
         install -d $kerneldir/dtb/overlays
@@ -5,6 +9,9 @@ do_install_append () {
             install -m 0644 ${DEPLOY_DIR_IMAGE}/overlays/*.dtbo $kerneldir/dtb/overlays
             install -m 0644 ${DEPLOY_DIR_IMAGE}/overlays.txt $kerneldir/dtb
         fi
+    elif [ "${KERNEL_IMAGETYPE}" = "fitImage" ]; then
+        install -d $kerneldir/dtb
+        install -m 0644 ${DEPLOY_DIR_IMAGE}/overlays.txt $kerneldir/dtb
     fi
 }
-do_install[depends] += "${@'device-tree-overlay-filter:do_deploy' if '${PREFERRED_PROVIDER_virtual/dtb}' else ''}"
+do_install[depends] += "${@'virtual/dtb:do_deploy' if '${PREFERRED_PROVIDER_virtual/dtb}' else ''}"

commit 3c356076792534a8a0f925dca6a92342a7ba1949

ostree-kernel-initramfs: add a condition check
A regression was introduced by 3f3faa9f:
[ ostree-kernel-initramfs: add ostree-devicetree-overlays package ]

Check if ${DEPLOY_DIR_IMAGE}/overlays.txt present before installing it,
not all machines deploys overlays.txt to ${DEPLOY_DIR_IMAGE}.

Related-to: TOR-1700

Signed-off-by: Ming Liu <ming.liu@toradex.com>

recipes-sota/ostree-kernel-initramfs/ostree-kernel-initramfs_%.bbappend
@@ -10,8 +10,10 @@ do_install_append () {
             install -m 0644 ${DEPLOY_DIR_IMAGE}/overlays.txt $kerneldir/dtb
         fi
     elif [ "${KERNEL_IMAGETYPE}" = "fitImage" ]; then
-        install -d $kerneldir/dtb
-        install -m 0644 ${DEPLOY_DIR_IMAGE}/overlays.txt $kerneldir/dtb
+        if [ -e ${DEPLOY_DIR_IMAGE}/overlays.txt ]; then
+            install -d $kerneldir/dtb
+            install -m 0644 ${DEPLOY_DIR_IMAGE}/overlays.txt $kerneldir/dtb
+        fi
     fi
 }
 do_install[depends] += "${@'virtual/dtb:do_deploy' if '${PREFERRED_PROVIDER_virtual/dtb}' else ''}"