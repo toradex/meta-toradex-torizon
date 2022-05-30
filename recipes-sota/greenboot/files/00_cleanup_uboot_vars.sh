93bc0f65eea70ae0b870fb0dfbd055b6408d361f
Rogerio Guerra Borin <rogerio.borin@toradex.com>
2022-05-30 13:15:03 -0300

    cleanup_uboot_vars: do not clean up u-boot vars if offline provisioned
    Add condition to avoid cleaning up u-boot variables in case the device
    is offline-provisioned; aktualizr will be responsible for doing that in
    such a case.
    
    Signed-off-by: Rogerio Guerra Borin <rogerio.borin@toradex.com>

--- recipes-sota/greenboot/files/00_cleanup_uboot_vars.sh
+++ recipes-sota/greenboot/files/00_cleanup_uboot_vars.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 UPGRADE_AVAILABLE=$(fw_printenv upgrade_available | cut -d '=' -f 2)
+DEVICE_PROVISIONED=$(test -e /var/sota/import/gateway.url -o -e /var/sota/import/director/root.json && echo '1')
 
 # if update in progress but device is not provisioned, aktualizr will be inactive,
 # so let's manually clean-up U-Boot variables to confirm the update
-if [ "$UPGRADE_AVAILABLE" = "1" -a ! -e /var/sota/import/gateway.url ]; then
+if [ "$UPGRADE_AVAILABLE" = "1" -a "$DEVICE_PROVISIONED" != "1" ]; then
     echo "Aktualizr-Torizon is inactive. Cleaning up U-Boot variables to confirm the update..."
     fw_setenv upgrade_available 0
     fw_setenv bootcount 0
