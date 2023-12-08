96d00a340d676dad4c6ddd89c5c0e4825d46ca17
Carlos Henrique Lima Melara <carlos.melara@toradex.com>
2023-12-08 15:07:44 -0300

    torizon-conf: use 4 spaces instead of tab to comply with codebase style
    Related-to: TCCP-20
    Signed-off-by: Carlos Henrique Lima Melara <carlos.melara@toradex.com>

--- recipes-core/torizon-conf/torizon-conf/containers-tags.sh
+++ recipes-core/torizon-conf/torizon-conf/containers-tags.sh
@@ -5,20 +5,20 @@ TORIZON_PURPOSE=$(grep -Po '(?<=\.0-)\w+' /etc/os-release | sort -u)
 
 # Common tags for all releases
 if [ "$TORIZON_PURPOSE" = "devel" ]; then
-	export CT_TAG_DEBIAN="rc-bookworm"
-	export CT_TAG_DOCKER_COMPOSE="rc"
-	export CT_TAG_WAYLAND_BASE="rc"
-	export CT_TAG_WESTON="rc"
-	export CT_TAG_WESTON_TOUCH_CALIBRATOR="rc"
-	export CT_TAG_PORTAINER_DEMO="rc"
-	export CT_TAG_QT5_WAYLAND="rc"
-	export CT_TAG_QT5_WAYLAND_EXAMPLES="rc"
-	export CT_TAG_TORIZONCORE_BUILDER="rc"
-	export CT_TAG_RT_TESTS="rc"
-	export CT_TAG_STRESS_TESTS="rc"
-	export CT_TAG_GRAPHICS_TESTS="rc"
-	export CT_TAG_CHROMIUM="rc"
-	export CT_TAG_COG="rc"
+    export CT_TAG_DEBIAN="rc-bookworm"
+    export CT_TAG_DOCKER_COMPOSE="rc"
+    export CT_TAG_WAYLAND_BASE="rc"
+    export CT_TAG_WESTON="rc"
+    export CT_TAG_WESTON_TOUCH_CALIBRATOR="rc"
+    export CT_TAG_PORTAINER_DEMO="rc"
+    export CT_TAG_QT5_WAYLAND="rc"
+    export CT_TAG_QT5_WAYLAND_EXAMPLES="rc"
+    export CT_TAG_TORIZONCORE_BUILDER="rc"
+    export CT_TAG_RT_TESTS="rc"
+    export CT_TAG_STRESS_TESTS="rc"
+    export CT_TAG_GRAPHICS_TESTS="rc"
+    export CT_TAG_CHROMIUM="rc"
+    export CT_TAG_COG="rc"
 else
     export CT_TAG_DEBIAN="3-bookworm"
     export CT_TAG_WAYLAND_BASE="3"
@@ -46,25 +46,25 @@ fi
 
 # Machine-specific tags
 case "$MACHINE" in
-	apalis-imx6|colibri-imx7-emmc|colibri-imx6|colibri-imx6ull-emmc|qemuarm64|genericx86-64)
-		;;
+    apalis-imx6|colibri-imx7-emmc|colibri-imx6|colibri-imx6ull-emmc|qemuarm64|genericx86-64)
+        ;;
 
-	apalis-imx8|colibri-imx8x|verdin-imx8mm|verdin-imx8mp)
-		if [ "$TORIZON_PURPOSE" = "devel" ]; then
-			export CT_TAG_QT6_WAYLAND="rc"
-			export CT_TAG_QT6_WAYLAND_EXAMPLES="rc"
-		else
-			export CT_TAG_QT6_WAYLAND="3"
-			export CT_TAG_QT6_WAYLAND_EXAMPLES="3"
-		fi
-		;;
-	verdin-am62)
-		if [ "$TORIZON_PURPOSE" = "devel" ]; then
-			export CT_TAG_QT6_WAYLAND="rc"
-			export CT_TAG_QT6_WAYLAND_EXAMPLES="rc"
-		else
-			export CT_TAG_QT6_WAYLAND="3"
-			export CT_TAG_QT6_WAYLAND_EXAMPLES="3"
-		fi
-		;;
+    apalis-imx8|colibri-imx8x|verdin-imx8mm|verdin-imx8mp)
+        if [ "$TORIZON_PURPOSE" = "devel" ]; then
+            export CT_TAG_QT6_WAYLAND="rc"
+            export CT_TAG_QT6_WAYLAND_EXAMPLES="rc"
+        else
+            export CT_TAG_QT6_WAYLAND="3"
+            export CT_TAG_QT6_WAYLAND_EXAMPLES="3"
+        fi
+        ;;
+    verdin-am62)
+        if [ "$TORIZON_PURPOSE" = "devel" ]; then
+            export CT_TAG_QT6_WAYLAND="rc"
+            export CT_TAG_QT6_WAYLAND_EXAMPLES="rc"
+        else
+            export CT_TAG_QT6_WAYLAND="3"
+            export CT_TAG_QT6_WAYLAND_EXAMPLES="3"
+        fi
+        ;;
 esac
